bbc0bd5c52f1c9445a09c3b6ec439cce8a41874e
AndresVivass <a.m.vivas.rodriguez@st.hanze.nl>
2018-06-27 13:55:15 +0200

    Update final.sh

--- final.sh
+++ final.sh
@@ -1,5 +1,5 @@
 #=============================
-# System updates
+# Primary System updates
 #=============================
 sudo apt-get update
 sudo apt-get upgrade
@@ -9,9 +9,33 @@ sudo apt-get upgrade
 #=============================
 
 
+#=======================================
+# Installing Salt-Master
+#=======================================
+curl -L https://bootstrap.saltstack.com -o install_salt.sh
+sudo sh install_salt.sh -M
+
+# Config file:
+
+#==========================================
+# Installing prerequired software (Manual)
+#==========================================
+apt-get -y install php
+apt-get -y install apache2
+apt-get -y install php-mysql
+apt-get -y install php-snmp
+apt-get -y install php-xml
+apt-get -y install snmp
+apt-get -y install snmpd
+apt-get -y install snmp-mibs-downloader
+apt-get -y install rrdtool
+apt-get -y install mysql-server
+
 
 
 
 #=======================================
-# Installing monitor service (cacti) 
+# Installing Docker
 #=======================================
+
+
